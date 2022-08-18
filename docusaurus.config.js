d49fe26bebc50f6dc38981bb78cb5dc96e91b832
ManhLV <ManhLV@sphinxjsc.com>
2022-08-19 00:10:48 +0700

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "LeeManh",
   tagline: "Chả biết viết gì 🤣",
-  url: "https://incredible-squirrel-f5c366.netlify.app", // Url to your site with no trailing slash
+  url: "https://gorgeous-semolina-714637.netlify.app", // Url to your site with no trailing slash
   baseUrl: "/", // Base directory of your site relative to your repo
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
